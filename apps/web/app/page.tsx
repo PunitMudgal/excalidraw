6ec25d1f06b57a6fe26f3c0ab3f530e12ca6bb80
Punit Sharma <ssmudgal01@gmail.com>
2026-04-30 21:57:05 +0530

    refactor: enhance layout of input and button in main page for better user experience

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,14 +9,18 @@ const page = () => {
 
   return (
     <div className="flex w-screen h-screen justify-center items-center">
-      <input
-        value={roomId || ""}
-        onChange={(e) => setRoomId(e.target.value)}
-        type="text"
-        placeholder="Enter Room ID..."
-      />
+      <div className="flex flex-col gap-4">
+        <input
+          value={roomId || ""}
+          onChange={(e) => setRoomId(e.target.value)}
+          type="text"
+          placeholder="Enter Room ID..."
+        />
 
-      <button onClick={() => router.push(`/room/${roomId}`)}>Join Room</button>
+        <button onClick={() => router.push(`/room/${roomId}`)}>
+          Join Room
+        </button>
+      </div>
     </div>
   );
 };
